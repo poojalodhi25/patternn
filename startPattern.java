ac316c8efbc7705496501d52246efdec62246322
Pooja Lodhi <lodhipooja952@gmail.com>
2026-08-19 15:17:32 +0530

    first commit

---
+++ startPattern.java
@@ -0,0 +1,16 @@
+public class startPattern{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in){
+            System.out.println("Enter the rows & col");
+            int n=sc.nextInt();
+            int m=sc.nextInt();
+            for(int i=0; i<n; i++){
+                for(j=1; i<m;j++){
+                    System.out.print(*);
+                }
+                System.out.println();
+            }
+
+        }
+    }
+}
